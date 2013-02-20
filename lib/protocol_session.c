44d33f0a0b279f1e16eaa47242abcea0dc5a7efe
Jarib Rahman <jarib@bu.edu>
2013-02-19 22:53:58 -0500

    Changes to protocol_session.c

--- lib/protocol_session.c
+++ lib/protocol_session.c
@@ -337,7 +337,7 @@ proto_session_rpc(Proto_Session *s)
   // ADD CODE
 
   s->slen = sizeof(s->shdr);
-  net_writen(s->fd, &(s->shdr), s->slen)
+  net_writen(s->fd, &(s->shdr), s->slen);
   
   s->rlen = sizeof(s->rhdr);
   if (net_readn(s->fd, &(s->rhdr), s->rlen) >= 0) 
