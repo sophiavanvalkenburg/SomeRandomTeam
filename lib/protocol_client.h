fd1bf926a686e039bd2601c958181170cebb18a7
Zheng Fan <fan0114@gmail.com>
2013-02-22 15:25:05 -0500

    Update lib/protocol_client.h

--- lib/protocol_client.h
+++ lib/protocol_client.h
@@ -43,6 +43,6 @@ extern int proto_client_set_event_handler(Proto_Client_Handle ch,
 
 // client side protocol rpc's
 extern int proto_client_hello(Proto_Client_Handle ch);
-extern int proto_client_move(Proto_Client_Handle ch, char d);
+extern int proto_client_move(Proto_Client_Handle ch, int tp, int move);
 extern int proto_client_goodbye(Proto_Client_Handle ch);
 #endif
